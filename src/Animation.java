3698ce34a5c978ea41534de3a0ee85f3ac00e30d
Kappicht.Jannik <Kappicht.Jannik@DESKTOP-F25JDAB.2631.skool.net>
2018-11-15 09:09:49 +0100

    Initial Commit

--- src/Animation.java
+++ src/Animation.java
@@ -1,9 +1,78 @@
 
 public class Animation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	String name;
+	//int col=3;
+	//int row=3;
 
+	public Jannik(String name){
+	  this.name=name;
 	}
 
-}
+	public void janniksAnimation(){
+	  animation0();
+	  animation();
+	  animation2();
+	  animation3();
+	}
+
+	public void animation0(){
+		  int i=15;
+		  for (int col= 3; col>=1; col--){
+		        for(int row=3; row>=1;row--){
+		        lightMatrix.getLight(col, row).setColor(new Color (i));
+		        lightMatrix.getLight(col,row).sendCurrentColor();
+		        delay (1000);
+
+		    }
+		  }
+		} 
+	
+	public void animation(){
+	  int i=19;
+	  for (int col= 3; col>=1; col--){
+	        for(int row=3; row>=1;row--){
+	        lightMatrix.getLight(col, row).setColor(new Color (i));
+	        lightMatrix.getLight(col,row).sendCurrentColor();
+	        delay (1000);
+
+	    }
+	  }
+	} 
+
+	public void animation2(){
+		  int i=21;
+		  for (int col= 3; col>=1; col--){
+		        
+		        lightMatrix.getLight(col,2).setColor(new Color (i));
+		        lightMatrix.getLight(col,2).sendCurrentColor();
+		        delay (1000);
+
+		    }
+		  }    
+	
+	public void animation3(){
+		  int i=20;
+		  for (int col= 3; col>=1; col--){
+		       
+		        lightMatrix.getLight(col,2).setColor(new Color (i));
+		        lightMatrix.getLight(col,2).sendCurrentColor();
+		        delay (1000);
+
+    	  }
+		}
+	
+	public void animation4(){
+		  int i=20;
+		  for (int col= 3; col>=1; col--){
+		        for(int row=3; row>=1;row--){
+		        lightMatrix.getLight(col, row).setColor(new Color (i));
+		        lightMatrix.getLight(col,row).sendCurrentColor();
+		        delay (1000);
+
+		    }
+		  }
+		}
+
+
+	}
